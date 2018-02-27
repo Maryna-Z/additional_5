7b31c8d88470ef91dd3350d5fc622124391b8edf
Marina-Z <33508772+Maryna-Z@users.noreply.github.com>
2018-02-27 23:58:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var openBrackets = [];
+    var closeBrackets = [];
+    var result = 0;
+    var strLength = str.length;
+    var configLenght = bracketsConfig.length;
+    var temp = [];
+
+    for (var i = 0; i < configLenght; i++){
+        openBrackets.push(bracketsConfig[i][0]);
+        closeBrackets.push(bracketsConfig[i][1]);
+    }
+
+    var openLenght = openBrackets.length;
+
+    for (var i = 0; i < strLength; i++){
+        for (var j = 0; j < openLenght; j++){
+            if (str[i] == openBrackets[j]){
+                result++;
+            }
+            if (str[i] == closeBrackets[j]){
+                result--;
+            }
+        }
+        if (result < 0) return false;
+    }
+    if (result == 0){
+        return true;
+    } else {
+        return false;
+    }
 }
